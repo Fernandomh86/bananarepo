2294ba7284d1e2f1b2fb2ea5ce2c3ed9a87ea77d
mdguay123 <danielm_95@hotamial.com>
2017-12-21 12:33:28 +0100

    Packages servlet + bean added. All classes reorganized. Added dependency to pom.xml javax.servlet-api to javax.servlet

---
+++ bananarepo/src/main/java/com/generation/jwd/servlets/taskServlet.java
@@ -0,0 +1,56 @@
+package com.generation.jwd.servlets;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.generation.jwd.beans.Task;
+/**
+ * Servlet implementation class taskServlet
+ */
+@WebServlet("/taskServlet")
+public class taskServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public taskServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	
+    }
+        // TODO Auto-generated method stub
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        
+         Task newTask = new Task();
+         newTask.setName(request.getParameter("name"));
+         newTask.setDescription(request.getParameter("description"));
+         newTask.setNotes(request.getParameter("notes"));
+         newTask.setInitialDate(request.getParameter("initialDate"));
+         newTask.setEndDate(request.getParameter("endDate"));
+         //newTask.setResponsable(request.getParameter("responsable"));
+
+            request.getRequestDispatcher("user_home.jsp").forward(request, response); // send us to user_home.jsp
+            request.setAttribute("newTaskName", newTask.getName()); //set attributes for the new task
+            request.setAttribute("newTaskInitialDate", newTask.getInitialDate());
+            request.setAttribute("newTaskEndDate", newTask.getEndDate());
+            response.sendRedirect("/homeServlet"); // change taskServlet to homeServlet
+            
+//        mysession.getAttribute();//PUT IN HOMESERVLET!!!!!!!!!!!!!!!
+            
+    }
+}
+ 
